5b070478289bfae01ea9307ad7a18bcbb1449858
sholahuddin <sholahuddin@sholahuddins-MacBook-Pro.local>
2017-08-16 09:25:54 +0700

    first commit

---
+++ index.vr.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import {
+  AppRegistry,
+  asset,
+  Pano,
+  Text,
+  View,
+  Image,
+} from 'react-vr';
+
+export default class WelcomeToVR extends React.Component {
+  render() {
+    return (
+      <View>
+        <Pano source={asset('chess-world.jpg')}/>
+        {/*<Text
+          style={{
+            backgroundColor: '#777879',
+            fontSize: 0.8,
+            fontWeight: '400',
+            layoutOrigin: [0.5, 0.5],
+            paddingLeft: 0.2,
+            paddingRight: 0.2,
+            textAlign: 'center',
+            textAlignVertical: 'center',
+            transform: [{translate: [0, 0, -3]}],
+          }}>
+          Mekar
+        </Text>*/}
+        {/*<Image
+          style={{
+            backgroundColor: '#777879',
+            fontSize: 0.8,
+            fontWeight: '400',
+            layoutOrigin: [0.5, 0.5],
+            paddingLeft: 0.2,
+            paddingRight: 0.2,
+            textAlign: 'center',
+            textAlignVertical: 'center',
+            transform: [{translate: [0, 0, -3]}],
+          }}
+          source={require('./logo.png')}
+        />*/}
+        {/**/}<Image source={require('./logo.png')}
+               style={{
+                // backgroundColor: '#777879',
+            fontSize: 0.8,
+            fontWeight: '400',
+            layoutOrigin: [0.5, 0.5],
+            paddingLeft: 0.2,
+            paddingRight: 0.2,
+            textAlign: 'center',
+            textAlignVertical: 'center',
+            transform: [{translate: [0, 0, -3]}],
+            width: 3.5, height: 1}} />
+      </View>
+    );
+  }
+};
+/*
+const styles = {
+  icon: {
+    backgroundColor: '#777879',
+    fontSize: 0.8,
+    fontWeight: '400',
+    layoutOrigin: [0.5, 0.5],
+    paddingLeft: 0.2,
+    paddingRight: 0.2,
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    transform: [{translate: [0, 0, -3]}],
+  }
+}
+*/
+AppRegistry.registerComponent('WelcomeToVR', () => WelcomeToVR);
